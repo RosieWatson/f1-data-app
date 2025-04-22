1e9c8a4e9b5b35c705b97b57993397c24bad8d4f
Rosie Watson <rosie.w@zeroheight.com>
2025-04-22 21:12:02 +0100

    feat: fetch driver standings data

---
+++ src/routes/+page.ts
@@ -0,0 +1,10 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	const res = await fetch('https://api.jolpi.ca/ergast/f1/2025/driverstandings/?format=json');
+	const response = await res.json();
+
+	const driverStandings = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+	console.log({ driverStandings });
+	return { driverStandings };
+};
