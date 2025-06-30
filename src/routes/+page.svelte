0a0a1f870a36c7c2f3b22de6cd7f9c249e1295e1
RosieWatson <rosiewatson@icloud.com>
2025-06-30 21:20:55 +0100

    chore: clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
-
-	console.log('do I have data??', data)
 </script>
 
 <div class="w-screen px-10 py-6">
-<h3>Race: Austria</h3>
-<h3>Driver: Lando Norris</h3>
+<h3><b>Race:</b> Austria</h3>
+<h3><b>Driver:</b> Lando Norris</h3>
+
+<hr>
 
 <span><b>Sector 1:</b>{data.duration_sector_1}<span><br/>
 <span><b>Sector 2:</b>{data.duration_sector_2}<span><br/>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,7 +5,6 @@ export const load: PageLoad = async ({ fetch, params }) => {
 		'https://api.openf1.org/v1/laps?session_key=9955&driver_number=4&lap_number=1'
 	);
 	const response = await res.json();
-	console.log(response);
 
 	const { duration_sector_1, duration_sector_2, duration_sector_3, lap_duration, st_speed } =
 		response[0];
