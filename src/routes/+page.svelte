73ee320655fd01bc2abbb358af4ed8f9ac62910b
Rosie <rosie@nicks-air-2.home>
2025-06-30 21:06:45 +0100

    feat: update to use new API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
-	import Card from '../lib/components/Card.svelte';
-	import ConstructorsTableRow from '../lib/components/ConstructorsTableRow.svelte';
-	import TableRow from '../lib/components/TableRow.svelte';
-
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	console.log('do I have data??', data)
 </script>
 
 <div class="w-screen px-10 py-6">
+<h3>Race: Austria</h3>
+<h3>Driver: Lando Norris</h3>
 
+<span><b>Sector 1:</b>{data.duration_sector_1}<span><br/>
+<span><b>Sector 2:</b>{data.duration_sector_2}<span><br/>
+<span><b>Sector 3:</b>{data.duration_sector_3}<span><br/>
+
+
+<span><b>Lap time:</b>{data.lap_duration}<span><br/>
+
+
+<span><b>Speed trap:</b>{data.st_speed}<span>
 </div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,44 +1,20 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('https://api.jolpi.ca/ergast/f1/2025/driverstandings/?format=json');
+export const load: PageLoad = async ({ fetch, params }) => {
+	const res = await fetch(
+		'https://api.openf1.org/v1/laps?session_key=9955&driver_number=4&lap_number=1'
+	);
 	const response = await res.json();
+	console.log(response);
 
-	const driverStandings = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-	const topDriver = driverStandings.find((ds: { position: string }) => ds.position === '1');
-	const secondDriver = driverStandings.find((ds: { position: string }) => ds.position === '2');
-	const thirdDriver = driverStandings.find((ds: { position: string }) => ds.position === '3');
-	const fourthDriver = driverStandings.find((ds: { position: string }) => ds.position === '4');
-	const fifthDriver = driverStandings.find((ds: { position: string }) => ds.position === '5');
-	const maxPoints = topDriver.points;
+	const { duration_sector_1, duration_sector_2, duration_sector_3, lap_duration, st_speed } =
+		response[0];
 
-	const conRes = await fetch('http://api.jolpi.ca/ergast/f1/2025/constructorstandings/');
-	const conResponse = await conRes.json();
-
-	const constructorsStandings =
-		conResponse.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-	const topCons = constructorsStandings.find((ds: { position: string }) => ds.position === '1');
-	const secondCons = constructorsStandings.find((ds: { position: string }) => ds.position === '2');
-	const thirdCons = constructorsStandings.find((ds: { position: string }) => ds.position === '3');
-	const fourthCons = constructorsStandings.find((ds: { position: string }) => ds.position === '4');
-	const fifthCons = constructorsStandings.find((ds: { position: string }) => ds.position === '5');
-	const maxConsPoints = topCons.points;
-
-	console.log({ topCons });
 	return {
-		driverStandings,
-		maxPoints,
-		topDriver,
-		secondDriver,
-		thirdDriver,
-		fourthDriver,
-		fifthDriver,
-		constructorsStandings,
-		topCons,
-		secondCons,
-		thirdCons,
-		fourthCons,
-		fifthCons,
-		maxConsPoints
+		duration_sector_1,
+		duration_sector_2,
+		duration_sector_3,
+		lap_duration,
+		st_speed
 	};
 };
