488734113351bf3a08d8dab05b3a8a89b89b9df9
RosieWatson <rosiewatson@icloud.com>
2025-06-30 22:19:56 +0100

    feat: show basic data for laps"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,116 @@
 <script lang="ts">
+	import { LinkedChart, LinkedValue } from 'svelte-tiny-linked-charts';
+
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	let sector1: Record<string, number> = $state.raw({});
+	let sector2: Record<string, number> = $state.raw({});
+	let sector3: Record<string, number> = $state.raw({});
+	let lapTime: Record<string, number> = $state.raw({});
+	let speedTrap: Record<string, number> = $state.raw({});
+
+	if (data.laps.length > 0) {
+		data.laps.forEach((lap) => {
+			sector1[`Lap ${lap.lap_number}`] = lap.duration_sector_1 ?? 0;
+			sector2[`Lap ${lap.lap_number}`] = lap.duration_sector_2 ?? 0;
+			sector3[`Lap ${lap.lap_number}`] = lap.duration_sector_3 ?? 0;
+			lapTime[`Lap ${lap.lap_number}`] = lap.lap_duration ?? 0;
+			speedTrap[`Lap ${lap.lap_number}`] = lap.st_speed ?? 0;
+		});
+	}
 </script>
 
 <div class="w-screen px-10 py-6">
-<h3><b>Race:</b> Austria</h3>
-<h3><b>Driver:</b> Lando Norris</h3>
+	<h3><b>Race:</b> Austria</h3>
+	<h3><b>Driver:</b> Lando Norris</h3>
 
-<hr>
+	<hr />
 
-<span><b>Sector 1:</b>{data.duration_sector_1}<span><br/>
-<span><b>Sector 2:</b>{data.duration_sector_2}<span><br/>
-<span><b>Sector 3:</b>{data.duration_sector_3}<span><br/>
+	<div>
+		<div class="data-row">
+			<div>
+				<h4>Sector 1</h4>
+				<LinkedValue uid="sector-1" />
+			</div>
+			<LinkedChart
+				data={sector1}
+				linked="link-1"
+				uid="sector-1"
+				grow
+				fadeOpacity={0.25}
+				fill="#cc3850"
+				gap={0.25}
+			/>
+		</div>
+		<br />
 
+		<div class="data-row">
+			<div>
+				<h4>Sector 2</h4>
+				<LinkedValue uid="sector-2" />
+			</div>
+			<LinkedChart
+				data={sector2}
+				linked="link-1"
+				uid="sector-2"
+				grow
+				fadeOpacity={0.25}
+				fill="#cc3850"
+				gap={0.25}
+			/>
+		</div>
+		<br />
 
-<span><b>Lap time:</b>{data.lap_duration}<span><br/>
+		<div class="data-row">
+			<div>
+				<h4>Sector 3</h4>
+				<LinkedValue uid="sector-3" />
+			</div>
+			<LinkedChart
+				data={sector3}
+				linked="link-1"
+				uid="sector-3"
+				grow
+				fadeOpacity={0.25}
+				fill="#cc3850"
+				gap={0.25}
+			/>
+		</div>
+		<br />
 
+		<div class="data-row">
+			<div>
+				<h4>Lap time</h4>
+				<LinkedValue uid="lap-time" />
+			</div>
+			<LinkedChart
+				data={lapTime}
+				linked="link-1"
+				uid="lap-time"
+				grow
+				fadeOpacity={0.25}
+				fill="#cc3850"
+				gap={0.25}
+			/>
+		</div>
+		<br />
 
-<span><b>Speed trap:</b>{data.st_speed}<span>
+		<div class="data-row">
+			<div>
+				<h4>Speed trap</h4>
+				<LinkedValue uid="speed-trap" />
+			</div>
+			<LinkedChart
+				data={speedTrap}
+				linked="link-1"
+				uid="speed-trap"
+				grow
+				fadeOpacity={0.25}
+				fill="#cc3850"
+				gap={0.25}
+			/>
+		</div>
+	</div>
 </div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,19 +1,22 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
-	const res = await fetch(
-		'https://api.openf1.org/v1/laps?session_key=9955&driver_number=4&lap_number=1'
-	);
-	const response = await res.json();
+	const sessionId = 9955; // Hardcode to Austria 2025
+	const driverNumber = 4; // Hardcode to Lando Norris
+	const noLaps = 71; // Hardcode to Austria laps
 
-	const { duration_sector_1, duration_sector_2, duration_sector_3, lap_duration, st_speed } =
-		response[0];
+	let laps: any[] = [];
+
+	for (let i = 1; i <= noLaps / 3; i++) {
+		const res = await fetch(
+			`https://api.openf1.org/v1/laps?session_key=${sessionId}&driver_number=${driverNumber}&lap_number=${i}`
+		);
+		const response = await res.json();
+
+		laps = [...laps, response[0]];
+	}
 
 	return {
-		duration_sector_1,
-		duration_sector_2,
-		duration_sector_3,
-		lap_duration,
-		st_speed
+		laps
 	};
 };
